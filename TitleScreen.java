8d1d1965b10237074081283ad65866da7dfb3e1a
howhi <howhiisahop@gmail.com>
2024-05-09 10:40:09 -0400

    Adjusted Title Screen Words and Added Unicode

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("The Elephant", 60);
+    Label titleLabel = new Label("The Elephant", 70);
 
     /**
      * Constructor for objects of class TitleScreen.
@@ -45,28 +45,9 @@ public class TitleScreen extends World
         Elephant elephant = new Elephant();
         addObject(elephant,476,71);
         elephant.setLocation(474,91);
-        Label label = new Label("Use <-- and --> to Move", 40);
-        addObject(label,200,238);
+        Label label = new Label("Use \u2190 and \u2192 to Move", 40);
+        addObject(label, 300, 300);
         Label label2 = new Label("Press <space> to Start", 40);
-        addObject(label2,219,333);
-        label.setLocation(320,269);
-        label2.setLocation(360,320);
-        label.setLocation(296,255);
-        label2.setLocation(237,326);
-        label2.setLocation(330,329);
-        label2.setLocation(293,324);
-        label.setLocation(446,281);
-        label2.setLocation(401,322);
-        label.setLocation(372,287);
-        label2.setLocation(302,319);
-        label.setLocation(287,282);
-        label.setLocation(338,278);
-        label.setLocation(292,272);
-        label.setLocation(323,273);
-        label2.setLocation(288,340);
-        label.setLocation(258,289);
-        label.setLocation(252,286);
-        label.setLocation(200,303);
-        label.setLocation(296,301);
+        addObject(label2, 300, 350);
     }
 }
