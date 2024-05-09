72a988525b259d3d5554aa822d6279ccf9da8be9
howhi <howhiisahop@gmail.com>
2024-05-09 10:49:35 -0400

    Added Space Background, Made Game Harder

--- Apple.java
+++ Apple.java
@@ -12,10 +12,11 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int speed = 1;
     public void act()
     {
         // Add your action code here.
-        setLocation(getX(), getY() + 1);
+        setLocation(getX(), getY() + speed);
         
         // Remove apple and draw game over when apple gets to bottom
         MyWorld world = (MyWorld) getWorld();
@@ -24,5 +25,10 @@ public class Apple extends Actor
             world.gameOver();
             world.removeObject(this);
         }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
--- Elephant.java
+++ Elephant.java
@@ -76,12 +76,12 @@ public class Elephant extends Actor
         // Add your action code here.
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-1);
+            move(-2);
             facing = "left";
         } 
         else if(Greenfoot.isKeyDown("right"))
         {
-            move(1);
+            move(2);
             facing = "right";
         }
         
