c6f97b3b2e270dc88f77e73edd3d35c84d92e992
howhi <howhiisahop@gmail.com>
2024-05-15 11:29:07 -0400

    Changed the Elephant Titlescreen size

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("The Elephant", 70);
+    Label titleLabel = new Label("The Elephant", 100);
 
     /**
      * Constructor for objects of class TitleScreen.
